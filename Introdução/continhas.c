4bc70df659c116483900e1defaeebe0098f64e02
M. Fernanda <n.nandcchi@gmail.com>
2025-01-14 21:44:05 -0300

    Update and rename Questão 1 to continhas.c

--- Introdução/continhas.c
+++ Introdução/continhas.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
-// “O homem que calculava”. Ambrósio trabalhava em uma empresa cuja missão era fazer cálculos de trajetória de foguete. A função dele era receber as fórmulas e os valores para efetuar os cálculos. Faça um programa que leia as variáveis (a, b e c) e imprima a resposta das fórmulas: 1) (a*b)/c, 2) a^2+b+5*c, 3) a*b*c+b+(c/3)5-1 e 4) (a*b*c)^3/2.
-
+/* Um homem trabalhava em uma empresa cuja missão era fazer cálculos de trajetória de foguete. A função dele era receber as fórmulas e os valores para efetuar os cálculos. Faça um programa que leia as variáveis (a, b e c) e imprima a resposta das fórmulas: 1) (a*b)/c, 2) a^2+b+5*c, 3) a*b*c+b+(c/3)5-1 e 4) (a*b*c)^3/2.
+*/
 int main () {
 
 	float a, b, c;
