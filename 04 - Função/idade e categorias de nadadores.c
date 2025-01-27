cd1cf656c49762c2f66424a020b5295cb1496d36
M. Fernanda <n.nandcchi@gmail.com>
2025-01-27 19:27:46 -0300

    Update idade e categorias de nadadores.c

--- 04 - Função/idade e categorias de nadadores.c
+++ 04 - Função/idade e categorias de nadadores.c
@@ -14,46 +14,7 @@ também por parâmetro, a categoria desse nadador de acordo com a tabela abaixo:
 - Idade: Maiores de 18 (Inclusive) e Categoria: Adulto.
 */
 
-void DeterminarCategoria (int idade, char categoria []) { // Função para determinar a categoria do nadador
-    
-	if (idade >= 5 && idade <= 7) {
-		
-        sprintf (categoria, "Infantil A");
-    }
-	else if (idade >= 8 && idade <= 10) {
-		
-        sprintf (categoria, "Infantil B");
-    }
-	else if (idade >= 11 && idade <= 13) {
-		
-        sprintf (categoria, "Juvenil A");
-    }
-	else if (idade >= 14 && idade <= 17) {
-        
-		sprintf (categoria, "Juvenil B");
-    }
-	else if (idade >= 18) {
-        
-		sprintf (categoria, "Adulto");
-    }
-	else {
-        sprintf (categoria, "Idade fora da faixa");
-    }
-}
 
-int main () {
-	
-    setlocale (LC_ALL, "Portuguese"); 
-
-    int idade;
-    char categoria [20];
-
-    printf ("Digite a idade do nadador: \n");
-    scanf ("%d", &idade);
-
-    DeterminarCategoria (idade, categoria);
-
-    printf ("Categoria do nadador: %s \n", categoria);
 
     return 0;
 }
