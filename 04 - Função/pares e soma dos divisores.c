dd8b5b2c712a9f07b376d756776a97b4dbd19f1d
M. Fernanda <n.nandcchi@gmail.com>
2025-01-27 19:28:12 -0300

    Update pares e soma dos divisores.c

--- 04 - Função/pares e soma dos divisores.c
+++ 04 - Função/pares e soma dos divisores.c
@@ -16,52 +16,6 @@ mesmo).
 4.4 Saída: A soma dos divisores. Exemplo: Para o valor 8: 1+2+4 = 7
 */
 
-int VerificaPositivo (int num) { // Função para verificar se um número é positivo
-    
-	return num > 0;
-}
-
-int SomaDivisores (int num) { // Função para calcular a soma dos divisores de um número (exceto ele mesmo)
-	
-    int soma = 0;
-
-    for (int i = 1; i < num; i++) {
-    	
-        if (num % i == 0) {         
-            soma = soma + i;              
-        }
-    }
-
-    return soma;
-}
-
-int main () {
-
-    setlocale (LC_ALL, "Portuguese");
-
-    int numeros [5]; // Array para armazenar os números
-    int soma;
-
-    printf ("Digite cinco números inteiros positivos: \n");
-
-    for (int i = 0; i < 5; i++) {
-    	
-        scanf ("%d", &numeros [i]);
-
-        while (!VerificaPositivo (numeros [i])) { 
-            
-			printf ("Número inválido! Digite um número inteiro positivo:\n");
-            scanf ("%d", &numeros [i]);
-        }
-    }
-
-    printf ("Soma dos divisores dos números informados (exceto eles mesmos): \n");
-
-    for (int i = 0; i < 5; i++) {
-        
-		soma = SomaDivisores(numeros [i]);
-        printf ("Número: %d -> Soma dos divisores: %d \n", numeros [i], soma);
-    }
 
     return 0;
 }
